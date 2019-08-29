9392f852dce2e438c48e5b7ef6463e408fa0f36f
Antonio Ruffolo <antonioruffolo2@gmail.com>
2019-08-29 21:32:08 +0200

    added swiftlint and removed warning and errors

--- DotsMatrix/DotsMatrix/ViewController.swift
+++ DotsMatrix/DotsMatrix/ViewController.swift
@@ -8,13 +8,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-  override func viewDidLoad() {
+class ViewController: UIViewController
+{
+  override func viewDidLoad()
+  {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
   }
-
-
 }
-
