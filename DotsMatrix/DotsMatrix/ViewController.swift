b2dba491d30097819739e66e7f13339a353b3b6a
Antonio Ruffolo <antonioruffolo2@gmail.com>
2019-08-29 21:26:48 +0200

    Project created

---
+++ DotsMatrix/DotsMatrix/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DotsMatrix
+//
+//  Created by Antonio Ruffolo on 29/08/2019.
+//  Copyright © 2019 AR. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
